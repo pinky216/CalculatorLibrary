c59b6cab41ea606f8ce70fdd2992337f46ce6e10
Pinky Kumari <pinky.kumari@signanthealth.com>
2021-05-20 19:18:12 +0530

    Adding MAthematical operations

---
+++ calculator/calculator.py
@@ -0,0 +1,16 @@
+"""
+
+Calculator library containing basic math operations
+"""
+
+
+def add(first_item, second_item):
+    return first_item + second_item
+
+
+def subtract(first_item, second_item):
+    return first_item - second_item
+
+
+print(add(4,6))
+print(subtract(9,8))
