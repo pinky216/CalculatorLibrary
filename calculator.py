b501cd020fda840058f19b5a7d1c8f55acc06f9b
Pinky Kumari <pinky.kumari@signanthealth.com>
2021-05-24 03:54:32 +0530

    Formatted file

--- calculator.py
+++ calculator.py
@@ -18,4 +18,8 @@ print(subtract(9,8))
 #pip install flake8 pytest pytest-cov
 #pip freeze > requirements.txt
 # To check the alignment we can run the flake8 --statistics
+# python -m pytest -v --cov .
 
+# .yaml file uses the serialization language called YAML designed to be pretty human readable
+
+# Docker is like minimal container which contians all the resources which can be required to run the code
--- test_calculator.py
+++ test_calculator.py
@@ -1,15 +1,6 @@
 import calculator
 
-"""
-Unit test for Calculator Library
 
-"""
-
-# python -m pytest -v --cov .
-
-# .yaml file uses the serialization language called YAML designed to be pretty human readable
-
-# Docker is like minimal container which contians all the resources which can be required to run the code
 class TestCalculator:
 
     def test_addition(self):
