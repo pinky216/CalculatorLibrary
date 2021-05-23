5bb4ba2aeb652e54b2467da968e33e0a3ec952e0
Pinky Kumari <pinky.kumari@signanthealth.com>
2021-05-24 03:57:43 +0530

    format file

--- calculator.py
+++ calculator.py
@@ -1,9 +1,3 @@
-"""
-
-Calculator library containing basic math operations
-"""
-
-
 def add(first_item, second_item):
     return first_item + second_item
 
@@ -12,11 +6,11 @@ def subtract(first_item, second_item):
     return first_item - second_item
 
 
-print(add(4,6))
-print(subtract(9,8))
+print(add(4, 6))
+print(subtract(9, 8))
 
-#pip install flake8 pytest pytest-cov
-#pip freeze > requirements.txt
+# pip install flake8 pytest pytest-cov
+# pip freeze > requirements.txt
 # To check the alignment we can run the flake8 --statistics
 # python -m pytest -v --cov .
 
