b0b9e5044ef7becc5906e898793e7b50a6c61b35
Pinky Kumari <pinky.kumari@signanthealth.com>
2021-05-24 04:10:23 +0530

    Added the multiply

--- calculator.py
+++ calculator.py
@@ -6,6 +6,10 @@ def subtract(first_item, second_item):
     return first_item - second_item
 
 
+def multiply(first_item, second_item):
+    return first_item * second_item
+
+
 print(add(4, 6))
 print(subtract(9, 8))
 
